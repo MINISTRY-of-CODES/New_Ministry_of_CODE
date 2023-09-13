6750884f701cb3b14abd45a5016f6ee09c01986e
Pama Lee <pama@pamalee.cn>
2023-09-13 19:01:50 +0800

    feat: 新增项目详情页面

---
+++ src/views/ProjectDetailView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import {useRouter} from "vue-router";
+import {onMounted, ref} from "vue";
+
+const router = useRouter();
+const id = ref("");
+const projectData = ref<any>({});
+const loadingProject = ref(true);
+
+onMounted(()=> {
+  id.value = <string>router.currentRoute.value.params.id;
+})
+</script>
+
+<template>
+<div style="margin: 20px">
+  <el-skeleton :loading="loadingProject" :rows="10">
+
+  </el-skeleton>
+</div>
+</template>
+
+<style scoped>
+
+</style>
