12be0fb1b9b32aeffb587db73530dbbde2a59570
GowayLee <1513624626@qq.com>
2023-09-30 14:40:42 +0800

    feat:新增成员详情页面左侧基本信息展示

--- src/views/MemberDetailView.vue
+++ src/views/MemberDetailView.vue
@@ -9,6 +9,19 @@ const loadingMember = ref(true);
 const infoURL = "https://raw.githubusercontent.com/MINISTRY-of-CODES/New_Ministry_of_CODE/master/static/members/" +
       id.value + "/info.json";
 
+// 检测屏幕大小
+const width = ref(window.innerWidth);
+
+onMounted(() => {
+  window.addEventListener('resize', () => {
+    width.value = window.innerWidth
+  })
+})
+
+const isLargescreen = computed(() => {
+  return (width.value > 770);
+})
+
 onMounted(async () => {
   memberInfo.value = await fetch(infoURL).then(res => res.json()).catch(err => {
     console.log(err);
@@ -21,6 +34,10 @@ onMounted(async () => {
   loadingMember.value = false;
 })
 
+//点击按钮跳转网址
+const goTo = (URL: string) => {
+  window.open(URL, "_blank");
+}
 
 </script>
 
@@ -47,7 +64,40 @@ onMounted(async () => {
           {{ item }}
         </p>
         <el-divider></el-divider>
+        <p style="
+        font-size: 24px;
+        font-weight: bold;
+        text-align: center;
+        margin-top: 35px;
+        ">
+          {{ memberInfo.profile.major }}
+        </p>
+        <p style="
+        font-size: 20px;
+        font-weight: bold;
+        text-align: center;
+        margin-top: 25px;
+        opacity: 0.7;
+        ">
+          {{ memberInfo.profile.grade }}
+        </p>
+        <p style="
+        font-size: 20px;
+        font-weight: bold;
+        text-align: center;
+        margin-top: 40px;
+        ">
+          {{ "Join in MOC on " + new Date(memberInfo.joinTime).toLocaleDateString() }}
+        </p>
+        <el-divider></el-divider>
+        <div v-for="item in memberInfo.website" :key="item" style="display: flex; justify-content: center;">
+          <el-button size="large" type="primary" @click="goTo(item.URL)" class="websiteBtn" 
+          :style="isLargescreen? 'width: 50%' : 'width: 100%'">
+            {{ item.name }}
+          </el-button>
+        </div>
         </el-col>
+
         <el-col :xs="22" :span="11">
         
         
@@ -60,3 +110,11 @@ onMounted(async () => {
     </el-skeleton>
   </div>
 </template>
+
+<style>
+.websiteBtn {
+  margin-bottom: 15px; 
+  font-size: 16px;
+}
+
+</style>
